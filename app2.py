dda64fb7d81ffc26bc531bf969dc8f89b9a5a515
AlbertoYnz <0270353@up.edu.mx>
2025-12-02 20:34:51 -0600

    Add button to clear conversation history
    Added a button to clear the conversation history.

--- app2.py
+++ app2.py
@@ -10,6 +10,8 @@ VECTOR_STORE_ID = "vs_6913baba995c81918b7f38c033955571"
 st.set_page_config(page_title="ALBot", page_icon="😎")
 st.title("ALBot - UP")
 st.caption("Resuelve dudas de todo tipo")
+st.button("Borrar conversacion")
+    st.session_state.messages = []
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -45,6 +47,7 @@ if user_input:
 
 
     
+
 
 
 
